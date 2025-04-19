c03975370483b7eb1104f40732f2f765bdf11576
Josiah Saunders <contact@josiahsaunders.com>
2025-04-19 07:52:58 -0600

    place in monkeydo folder

--- src/extension.ts
+++ src/extension.ts
@@ -28,10 +28,22 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
+    const wsFolder = vscode.workspace.workspaceFolders?.[0];
+    if (!wsFolder) {
+      vscode.window.showErrorMessage('No workspace folder found. Please open a workspace folder.');
+      return;
+    }
+
+    // create the .monkeydo folder if it doesn't already exist
+    const monkeydoFolder = path.join(wsFolder.uri.fsPath, '.monkeydo');
+    if (!fs.existsSync(monkeydoFolder)) {
+      fs.mkdirSync(monkeydoFolder);
+    }
+
     vscode.window.showInformationMessage('Monkey Do recording stopped!');
     const saveUri = await vscode.window.showSaveDialog({
       title: 'Save Monkey Do action log',
-      defaultUri: vscode.Uri.file(path.join((vscode.workspace.workspaceFolders?.[0].uri.fsPath || ''), document.filename)),
+      defaultUri: vscode.Uri.file(path.join(monkeydoFolder, document.filename)),
       filters: { 'Markdown Files': ['md'], 'All Files': ['*'] }
     });
     if (saveUri) {
